1432c720e1529b1ad9296071da5799e2770ca30e
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2023-03-17 08:43:28 +0100

    :sparkles: no ads in bign seach script

---
+++ scripts/mathis-gasparotto/noAdsInBingSearch.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         No ad results on bing search
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @updateURL    https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/mathis-gasparotto/noAdsInBingSearch.js
+// @downloadURL  https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/mathis-gasparotto/noAdsInBingSearch.js
+// @description  ui
+// @author       Mathis Gasparotto
+// @match        https://www.bing.com/search?q=*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bing.com
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    GM_addStyle(`
+    .b_ad.b_adTop {
+        display: none;
+    `)
+})();
