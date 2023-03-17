af7b446e7e5bd2c4931c6fc7822c07b3f1e8df61
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2023-03-17 08:54:11 +0100

    update test

--- scripts/mathis-gasparotto/noAdsInBingSearch.js
+++ scripts/mathis-gasparotto/noAdsInBingSearch.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         No ad results on bing search
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @updateURL    https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/mathis-gasparotto/noAdsInBingSearch.js
 // @downloadURL  https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/mathis-gasparotto/noAdsInBingSearch.js
 // @description  ui
@@ -17,4 +17,5 @@
     .b_ad.b_adTop {
         display: none;
     `)
+    console.log('voici la v0.2')
 })();
