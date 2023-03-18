f725a7ffdb036094377c19a6d93b957933344cd8
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2023-03-18 05:10:40 +0100

    :fire: remove test console.log

--- scripts/maggio/noAdsInBingSearch.js
+++ scripts/maggio/noAdsInBingSearch.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         No ad results on bing search
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @updateURL    https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/noAdsInBingSearch.js
 // @downloadURL  https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/noAdsInBingSearch.js
 // @description  ui
@@ -17,5 +17,4 @@
     .b_ad.b_adTop {
         display: none;
     `)
-    console.log('voici la v0.2')
 })();
