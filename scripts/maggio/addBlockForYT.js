ce50d9159a31731a3bb82629e8247ece2fb4f62c
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2023-03-31 21:33:43 +0200

    :adhesive_bandage: add selector for yt ads on search

--- scripts/maggio/addBlockForYT.js
+++ scripts/maggio/addBlockForYT.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ad block for YT
 // @namespace    https://github.com/Mathis-Gasparotto/tampermonkey-scripts/tree/master/scripts/maggio
-// @version      0.1.2
+// @version      0.2
 // @updateURL    https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/addBlockForYT.js
 // @downloadURL  https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/addBlockForYT.js
 // @description  disable top ads on YT
@@ -14,8 +14,13 @@
 
 (function() {
   GM_addStyle(`
-      .ytd-page-top-ad-layout-renderer, ytd-promoted-video-renderer, .ytd-promoted-video-renderer {
-          display: none;
-      }
+    .ytd-page-top-ad-layout-renderer, 
+    ytd-promoted-video-renderer, 
+    .ytd-promoted-video-renderer, 
+    ytd-in-feed-ad-layout-renderer, 
+    ytd-promoted-sparkles-web-renderer, 
+    .ytd-promoted-sparkles-web-renderer {
+      display: none;
+    }
   `)
 })();
