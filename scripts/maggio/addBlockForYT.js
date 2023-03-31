34f8af19f670d4a604f3b6c404f526bd3ae4bed9
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2023-03-31 21:40:20 +0200

    :art: remove useless spaces

--- scripts/maggio/addBlockForYT.js
+++ scripts/maggio/addBlockForYT.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ad block for YT
 // @namespace    https://github.com/Mathis-Gasparotto/tampermonkey-scripts/tree/master/scripts/maggio
-// @version      0.2.1
+// @version      0.2.2
 // @updateURL    https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/addBlockForYT.js
 // @downloadURL  https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/addBlockForYT.js
 // @description  disable top ads on YT
@@ -14,11 +14,11 @@
 
 (function() {
   GM_addStyle(`
-    .ytd-page-top-ad-layout-renderer, 
-    ytd-promoted-video-renderer, 
-    .ytd-promoted-video-renderer, 
-    ytd-in-feed-ad-layout-renderer, 
-    ytd-promoted-sparkles-web-renderer, 
+    .ytd-page-top-ad-layout-renderer,
+    ytd-promoted-video-renderer,
+    .ytd-promoted-video-renderer,
+    ytd-in-feed-ad-layout-renderer,
+    ytd-promoted-sparkles-web-renderer,
     .ytd-promoted-sparkles-web-renderer,
     ytd-ad-slot-renderer {
       display: none;
