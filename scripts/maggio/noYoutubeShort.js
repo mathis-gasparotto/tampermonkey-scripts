a3d84493ef9b6b144863360bad42e63dc91924eb
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2024-07-23 19:54:22 +0200

    fix: fix no yt shorts script

--- scripts/maggio/noYoutubeShort.js
+++ scripts/maggio/noYoutubeShort.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove Shorts from Youtube
 // @namespace    https://github.com/Mathis-Gasparotto/tampermonkey-scripts/tree/master/scripts/maggio
-// @version      0.2.1
+// @version      0.2.2
 // @updateURL    https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/noYoutubeShort.js
 // @downloadURL  https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/noYoutubeShort.js
 // @description  Save your time
@@ -17,17 +17,17 @@
   GM_addStyle(`
     /* Home page */
     ytd-rich-shelf-renderer[is-shorts] {
-      display: none;
+      display: none !important;
     }
 
     /* Side suggestions */
     ytd-reel-shelf-renderer {
-      display: none;
+      display: none !important;
     }
 
     /* Shorts btn on side bar */
     #guide-content a.ytd-guide-entry-renderer[title="Shorts"] {
-      display: none;
+      display: none !important;
     }
   `)
   if (window.location.href.includes('youtube.com/shorts')) {
