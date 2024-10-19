4eef8fa1c866f4de509d2d8732e8e14820889cf4
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2024-10-19 10:57:11 +0200

    fix(noBingSearchTargetBlank):make sure it works

--- scripts/maggio/noBingSearchTargetBlank.js
+++ scripts/maggio/noBingSearchTargetBlank.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Disable target="_blank" on Bing Search
 // @namespace    https://github.com/Mathis-Gasparotto/tampermonkey-scripts/tree/master/scripts/maggio
-// @version      0.1.1
+// @version      0.1.2
 // @updateURL    https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/noBingSearchTargetBlank.js
 // @downloadURL  https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/noBingSearchTargetBlank.js
 // @description  Prevent Bing Search from opening links in a new tab (on default settings) by removing the target="_blank" attribute on search results links
@@ -13,8 +13,10 @@
 // ==/UserScript==
 
 (function () {
-  const links = document.querySelectorAll('a[target="_blank"]');
-  links.forEach((link) => {
-    link.removeAttribute("target");
-  });
+  setTimeout(() => {
+    const links = document.querySelectorAll('a[target="_blank"]');
+    links.forEach((link) => {
+      link.removeAttribute("target");
+    });
+  }, 500);
 })();
