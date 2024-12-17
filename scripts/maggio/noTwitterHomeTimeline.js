f0124781dfef327c9f016fcf244a9487cd2d4b70
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2024-12-17 09:57:48 +0100

    feat(noTwitterHomeTimeline): hide premium & orgs tab buttons

--- scripts/maggio/noTwitterHomeTimeline.js
+++ scripts/maggio/noTwitterHomeTimeline.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Block Twitter home timeline
 // @namespace    https://github.com/Mathis-Gasparotto/tampermonkey-scripts/tree/master/scripts/maggio
-// @version      0.1.0
+// @version      0.2.0
 // @updateURL    https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/noTwitterHomeTimeline.js
 // @downloadURL  https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/noTwitterHomeTimeline.js
 // @description  Save your time
@@ -20,25 +20,34 @@
     nav[role="navigation"] a[href="/home"] {
       display: none;
     }
-  `)
+
+    /* Premium page button */
+    nav[role="navigation"] a[href="/i/premium_sign_up"] {
+      display: none;
+    }
+
+    /* Verified Orgs page button */
+    nav[role="navigation"] a[href="/i/verified-orgs-signup"] {
+      display: none;
+    }
+  `);
   if (window.onurlchange === null) {
-    window.addEventListener('urlchange', () => {
-      if (window.location.href.includes('/home')) {
+    window.addEventListener("urlchange", () => {
+      if (window.location.href.includes("/home")) {
         GM_addStyle(`
           /* Home main content */
           div[aria-label="Home timeline"] > div:last-child {
             display: none;
           }
-        `)
+        `);
       } else {
         GM_addStyle(`
           /* Home main content */
           div[aria-label="Home timeline"] > div:last-child {
             display: block;
           }
-        `)
+        `);
       }
-    })
+    });
   }
-
 })();
