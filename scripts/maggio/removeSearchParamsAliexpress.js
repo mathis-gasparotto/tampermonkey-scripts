de16824641516e1ae413d39363e7a3dbbbf40a61
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2025-03-20 18:11:30 +0100

    feat(removeSearchParamsAliexpress): add script to remove search params from path on Aliexpress

---
+++ scripts/maggio/removeSearchParamsAliexpress.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Remove search params from path on Aliexpress
+// @namespace    https://github.com/Mathis-Gasparotto/tampermonkey-scripts/tree/master/scripts/maggio
+// @version      0.1.0
+// @updateURL    https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/removeSearchParamsAliexpress.js
+// @downloadURL  https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/removeSearchParamsAliexpress.js
+// @description  bye bye huge links on Aliexpress
+// @author       Maggio
+// @match        https://*.aliexpress.com/*
+// @exclude      https://www.aliexpress.com/p/order/detail.html*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=aliexpress.com
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function () {
+  'use strict'
+
+  window.history.replaceState('object or string', 'Title', window.location.origin + window.location.pathname)
+})()
