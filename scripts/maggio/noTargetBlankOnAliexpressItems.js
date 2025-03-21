b1aea72f8a2494e1897338a423dd2f90d2f7f48c
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2025-03-21 13:22:47 +0100

    opti(noTargetBlankOnAliexpressItems): big opti (very low profile)

--- scripts/maggio/noTargetBlankOnAliexpressItems.js
+++ scripts/maggio/noTargetBlankOnAliexpressItems.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Disable target="_blank" on Aliexpress items
 // @namespace    https://github.com/Mathis-Gasparotto/tampermonkey-scripts/tree/master/scripts/maggio
-// @version      0.1.4
+// @version      0.1.5
 // @updateURL    https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/noTargetBlankOnAliexpressItems.js
 // @downloadURL  https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/noTargetBlankOnAliexpressItems.js
 // @description  Oui
@@ -11,51 +11,15 @@
 // @exclude      https://*.aliexpress.com/gcp/*
 // @exclude      https://*.aliexpress.com/ssr/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=aliexpress.com
-// @run-at       document-end
+// @run-at       document-start
 // ==/UserScript==
 
-;(function () {
-  let previousLinks = document.querySelectorAll('a[href*="aliexpress.com/item/"][target="_blank"]')
-  previousLinks.forEach((link) => {
-    link.removeAttribute('target')
-  })
-  // let previousFusionProducts = document.querySelectorAll('#fusionPageCard div[class*="fusion-page-card--listItem--"] div[class*="productItem--itemImg--"], #fusionPageCard div[class*="fusion-page-card--listItem--"] div[class*="productItem--itemInfoTitle--"]')
-  function checkForChanges() {
-    const currentLinks = document.querySelectorAll('a[href*="aliexpress.com/item/"][target="_blank"]')
-    // const currentFusionProducts = document.querySelectorAll('#fusionPageCard div[class*="fusion-page-card--listItem--"] div[class*="productItem--itemImg--"], #fusionPageCard div[class*="fusion-page-card--listItem--"] div[class*="productItem--itemInfoTitle--"]')
-
-    const sameLinks =
-      currentLinks.length === previousLinks.length &&
-      Object.values(currentLinks).every(function (value, index) {
-        return value === previousLinks[index]
-      })
-    // const sameFusionProducts = currentFusionProducts.length === previousFusionProducts.length && Object.values(currentFusionProducts).every(function(value, index) { return value === previousFusionProducts[index]})
-
-    // if (!sameLinks || !sameFusionProducts) {
-    if (!sameLinks) {
-      // console.log('Les liens ont été modifiés.')
-
-      currentLinks.forEach((link) => {
-        link.removeAttribute('target')
-      })
-
-      // const fusionProducts = document.querySelectorAll('#fusionPageCard div[class*="fusion-page-card--listItem--"] div[class*="productItem--itemImg--"], #fusionPageCard div[class*="fusion-page-card--listItem--"] div[class*="productItem--itemInfoTitle--"]')
-      // console.log("fusionProducts", fusionProducts)
-
-      previousLinks = currentLinks
-      // previousFusionProducts = fusionProducts
+(function () {
+  document.addEventListener('click', (e) => {
+    const aTag = e.target.closest("a[href*='aliexpress.com/item/'][target='_blank']")
+    if (aTag && aTag.href) {
+      e.preventDefault()
+      window.location.href = aTag.href
     }
-  }
-  setInterval(checkForChanges, 500)
-
-  // const config = { childList: true, subtree: true }
-  // const observer = new MutationObserver((mutationsList, observer) => {
-  //   for (let mutation of mutationsList) {
-  //     if (mutation.type === 'childList') {
-  //       console.log('Le contenu du DOM a été modifié.')
-
-  //     }
-  //   }
-  // })
-  // observer.observe(body, config)
+  })
 })()
