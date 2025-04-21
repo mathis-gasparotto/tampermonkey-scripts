9400d2bb2c4ae75f0487dfabd643136277914bfc
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2025-04-21 13:55:05 +0200

    fix(removeSearchParamsAliexpress): exclude showcredential page

--- scripts/maggio/removeSearchParamsAliexpress.js
+++ scripts/maggio/removeSearchParamsAliexpress.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove search params from path on Aliexpress
 // @namespace    https://github.com/Mathis-Gasparotto/tampermonkey-scripts/tree/master/scripts/maggio
-// @version      0.1.7
+// @version      0.1.8
 // @updateURL    https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/removeSearchParamsAliexpress.js
 // @downloadURL  https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/removeSearchParamsAliexpress.js
 // @description  bye bye huge links on Aliexpress
@@ -14,6 +14,7 @@
 // @exclude      https://www.aliexpress.com/p/tax-ui/index.html*
 // @exclude      https://m.aliexpress.com/p/refund-cancel/index.html*
 // @exclude      https://www.aliexpress.com/p/tracking/index.html*
+// @exclude      https://shoprenderview.aliexpress.com/credential/showcredential.htm*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=aliexpress.com
 // @grant        none
 // @run-at       document-start
