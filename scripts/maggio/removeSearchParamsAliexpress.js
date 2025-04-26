40e54ef91de738f9f8d3e63209526dda358102f6
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2025-04-26 21:02:50 +0200

    fix(removeSearchParamsAliexpress): exclude message center page

--- scripts/maggio/removeSearchParamsAliexpress.js
+++ scripts/maggio/removeSearchParamsAliexpress.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove search params from path on Aliexpress
 // @namespace    https://github.com/Mathis-Gasparotto/tampermonkey-scripts/tree/master/scripts/maggio
-// @version      0.1.8
+// @version      0.1.9
 // @updateURL    https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/removeSearchParamsAliexpress.js
 // @downloadURL  https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/removeSearchParamsAliexpress.js
 // @description  bye bye huge links on Aliexpress
@@ -15,6 +15,8 @@
 // @exclude      https://m.aliexpress.com/p/refund-cancel/index.html*
 // @exclude      https://www.aliexpress.com/p/tracking/index.html*
 // @exclude      https://shoprenderview.aliexpress.com/credential/showcredential.htm*
+// @exclude      https://www.aliexpress.com/p/message/index.html*
+// @exclude      https://msg.aliexpress.com/buyerMsgListNew.htm*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=aliexpress.com
 // @grant        none
 // @run-at       document-start
