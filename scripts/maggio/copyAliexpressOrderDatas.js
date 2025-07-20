1004dbd336529fca29cebae9dd22fbd0b937988d
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2025-07-20 16:23:54 +0200

    fix(copyAliexpressOrderDatas): fix for french display

--- scripts/maggio/copyAliexpressOrderDatas.js
+++ scripts/maggio/copyAliexpressOrderDatas.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copy Aliexpress order datas
 // @namespace    https://github.com/Mathis-Gasparotto/tampermonkey-scripts/tree/master/scripts/maggio
-// @version      0.1.5
+// @version      0.1.6
 // @updateURL    https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/copyAliexpressOrderDatas.js
 // @downloadURL  https://mathis-gasparotto.github.io/tampermonkey-scripts/scripts/maggio/copyAliexpressOrderDatas.js
 // @description  Copy Aliexpress order datas for past on a google sheet
@@ -23,32 +23,76 @@
   function addButton(orders) {
     if (!Array.from(orders).every((order) => ordersDone.includes(order))) clearInterval(interval)
 
+    function getOrderDate(order) {
+      const orderDateStr = order
+        .getElementsByClassName('order-item-header-right-info')[0]
+        .getElementsByTagName('div')[0]
+        .innerText.match(/:\s(.+)/)[1]
+
+      let orderDate = new Date(
+        orderDateStr
+      )
+      if (orderDate == 'Invalid Date') {
+        const months = {
+          "janv.": 0,
+          "févr.": 1,
+          "mars": 2,
+          "avr.": 3,
+          "mai": 4,
+          "juin": 5,
+          "juil.": 6,
+          "août": 7,
+          "sept.": 8,
+          "oct.": 9,
+          "nov.": 10,
+          "déc.": 11
+        }
+        const parts = orderDateStr.split(' ')
+        const day = parseInt(parts[0], 10)
+        const month = months[parts[1]]
+        const year = parseInt(parts[2], 10)
+        orderDate = new Date(year, month, day)
+      }
+      return orderDate.toLocaleDateString('en-US')
+    }
+
+    function getOrderNumber(order) {
+      return order.getElementsByClassName('order-item-header-right-info')[0].getElementsByTagName('div')[1].innerText.match(/(\d+)/)[0]
+    }
+
+    function getOrderTotal(order) {
+      return order
+        .getElementsByClassName('order-item-content-opt-price-total')[0]
+        .innerText.match(/(\d+),(\d{2})/)[0]
+        .replace(',', '.')
+    }
+
+    function getProductName(order) {
+      return order.getElementsByClassName('order-item-content-info-name')[0]?.innerText ?? ''
+    }
+
+    function getProductLink(order) {
+      return order.getElementsByClassName('order-item-content-info-name')[0]?.getElementsByTagName('a')[0].href ?? ''
+    }
+
+    function getProductQuantity(order) {
+      return order.getElementsByClassName('order-item-content-info-number-quantity')[0]?.innerText.match(/(\d+)/)[0] ?? ''
+    }
+
+    function getProductUnitPrice(order) {
+      return order
+        .getElementsByClassName('order-item-content-info-number')[0]
+        ?.getElementsByTagName('div')[0]
+        .innerText.match(/(\d+),(\d{2})/)[0]
+        .replace(',', '.')
+    }
+
     Array.from(orders)
       .filter((order) => !ordersDone.includes(order))
       .forEach((order) => {
         const header = order.getElementsByClassName('order-item-header-status')[0]
 
-        const orderNumber = order.getElementsByClassName('order-item-header-right-info')[0].getElementsByTagName('div')[1].innerText.match(/(\d+)/)[0]
-        const orderDate = new Date(
-          order
-            .getElementsByClassName('order-item-header-right-info')[0]
-            .getElementsByTagName('div')[0]
-            .innerText.match(/:\s(.+)/)[1]
-        ).toLocaleDateString('en-US')
-        const orderTotal = order
-          .getElementsByClassName('order-item-content-opt-price-total')[0]
-          .innerText.match(/(\d+),(\d{2})/)[0]
-          .replace(',', '.')
-
-        const productName = order.getElementsByClassName('order-item-content-info-name')[0]?.innerText ?? ''
-        const productLink = order.getElementsByClassName('order-item-content-info-name')[0]?.getElementsByTagName('a')[0].href ?? ''
-        const productQuantity = order.getElementsByClassName('order-item-content-info-number-quantity')[0]?.innerText.match(/(\d+)/)[0] ?? ''
-        const productUnitPrice =
-          order
-            .getElementsByClassName('order-item-content-info-number')[0]
-            ?.getElementsByTagName('div')[0]
-            .innerText.match(/(\d+),(\d{2})/)[0]
-            .replace(',', '.') ?? ''
+        const orderNumber = getOrderNumber(order)
 
         const copySpan = document.createElement('span')
         copySpan.style.fontSize = '13px'
@@ -59,6 +103,15 @@
         copySpan.innerText = 'Copier les données'
 
         copySpan.addEventListener('click', () => {
+          const orderDate = getOrderDate(order)
+
+          const orderTotal = getOrderTotal(order)
+
+          const productName = getProductName(order)
+          const productLink = getProductLink(order)
+          const productQuantity = getProductQuantity(order)
+          const productUnitPrice = getProductUnitPrice(order)
+
           navigator.clipboard.writeText(orderDate + tab + '=HYPERLINK("' + productLink + '", "' + productName + '")' + tab + '=HYPERLINK("https://www.aliexpress.com/p/order/detail.html?orderId=' + orderNumber + '", "' + orderNumber + '")' + tab + productUnitPrice + tab + productQuantity + tab + orderTotal)
 
           //const range = document.createRange()
